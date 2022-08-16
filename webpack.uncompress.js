334d8479b9308cba0ad6f6ac92d1904529389c8d
mkasumi <morita@appleple.com>
2022-08-16 16:54:20 +0900

    🛠ログアウト時にdocumentOutlinerがうまく動いてない問題を修正

--- webpack.uncompress.js
+++ webpack.uncompress.js
@@ -1,10 +1,10 @@
-const { merge } = require('webpack-merge');
+const webpackMerge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const pkg = require('./package.json');
 
 process.env.BABEL_ENV = 'production';
 
-module.exports = merge(common, {
+module.exports = webpackMerge.merge(common, {
   mode: 'production',
   resolve: {
     extensions: ['.js', '.ts', '.tsx']
