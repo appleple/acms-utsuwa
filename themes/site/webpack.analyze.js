eca822b30d6e1ff262dd2d680460265243f781e4
宇井 陸登 <ui@appleple.com>
2025-07-23 18:15:24 +0900

    調整

--- themes/site/webpack.analyze.js
+++ themes/site/webpack.analyze.js
@@ -6,9 +6,6 @@ process.env.BABEL_ENV = 'production';
 
 module.exports = merge(common, {
   mode: 'production',
-  resolve: {
-    extensions: ['.js', '.ts', '.tsx'],
-  },
   plugins: [
     new BundleAnalyzerPlugin(),
   ],
